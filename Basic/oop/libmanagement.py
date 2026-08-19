14b1d2e56cb7136d537de001f7390b8c1a84b602
ariguchi <218999881+ariguchi@users.noreply.github.com>
2026-08-20 01:17:16 +0530

    Add Books, Members, and Library classes

--- Basic/oop/libmanagement.py
+++ Basic/oop/libmanagement.py
@@ -1,1 +1,19 @@
 
+class Books:
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.borrowed = false
+
+class Members:
+
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+class Library:
+
+    def __init__(self ):
+        self.books = []
+        self.members = []
